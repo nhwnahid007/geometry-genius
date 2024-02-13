218d811e580a405553deede19fc00ac09ee76e78
nhwnahid007 <nhwnahid@gmail.com>
2024-02-13 12:44:12 +0600

    Added rectangle card and calculation

--- scripts/rectangle-calculator.js
+++ scripts/rectangle-calculator.js
@@ -3,15 +3,19 @@ function calculateRectangleArea() {
   const rectangleWidthInput = document.getElementById("rectangle-width");
   const rectangleWidthText = rectangleWidthInput.value;
   const width = parseFloat(rectangleWidthText);
-  console.log(width);
+  // console.log(width);
 
   //get length
 
   const rectangleLengthInput = document.getElementById("rectangle-length");
   const rectangleLengthText = rectangleLengthInput.value;
   const length = parseFloat(rectangleLengthText);
-  console.log(length);
+  // console.log(length);
 
   const area = width * length;
-  console.log(area);
+  console.log("Area of the rectangle is:", area);
+
+  //display area
+  const rectangleAreaSpan = document.getElementById("rectangle-area");
+  rectangleAreaSpan.innerText = area;
 }
